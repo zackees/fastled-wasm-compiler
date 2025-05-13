4e139225a6942dd4e6c569388e3a1012ceb5b774
zackees <z@zackees.com>
2025-05-13 00:18:37 -0700

    Add argparse for CLI arguments in CliArgs class

--- src/fastled_wasm_compiler/cli.py
+++ src/fastled_wasm_compiler/cli.py
@@ -2,15 +2,80 @@
 Main entry point.
 """
 
+import argparse
 import sys
+from dataclasses import dataclass
+from pathlib import Path
 
 from fastled_wasm_compiler.run import Args, run
 
 
+@dataclass
+class CliArgs:
+    compiler_root: Path
+    assets_dirs: Path
+    mapped_dir: Path
+    keep_files: bool
+    profile: bool
+    debug: bool
+    quick: bool
+    release: bool
+
+    @staticmethod
+    def parse_args() -> "CliArgs":
+        return _parse_args()
+
+
+def _parse_args() -> CliArgs:
+    parser = argparse.ArgumentParser(description="Compile FastLED for WASM")
+
+    parser.add_argument("--compiler-root", type=Path, required=True)
+    parser.add_argument(
+        "--assets-dirs",
+        type=Path,
+        required=True,
+        help="directory where index.html, index.js, etc are kept",
+    )
+    parser.add_argument(
+        "--mapped-dir",
+        type=Path,
+        default="/mapped",
+        help="Directory containing source files (default: /mapped)",
+    )
+    parser.add_argument(
+        "--keep-files", action="store_true", help="Keep source files after compilation"
+    )
+    parser.add_argument(
+        "--profile", action="store_true", help="Enable profiling of the build system"
+    )
+    parser.add_argument("--debug", action="store_true", help="Enable debug mode")
+    parser.add_argument("--quick", action="store_true", help="Enable quick mode")
+    parser.add_argument("--release", action="store_true", help="Enable release mode")
+
+    args = parser.parse_args()
+    return CliArgs(**vars(args))
+
+
 def main() -> int:
     """Main entry point for the template_python_cmd package."""
-    args = Args.parse_args()
-    return run(args)
+    cli_args = CliArgs.parse_args()
+    full_args: Args = Args(
+        compiler_root=cli_args.compiler_root,
+        assets_dirs=cli_args.assets_dirs,
+        mapped_dir=cli_args.mapped_dir,
+        keep_files=cli_args.keep_files,
+        only_copy=False,
+        only_insert_header=False,
+        only_compile=False,
+        profile=cli_args.profile,
+        disable_auto_clean=False,
+        no_platformio=False,
+        debug=cli_args.debug,
+        quick=cli_args.quick,
+        release=cli_args.release,
+    )
+    rtn = run(full_args)
+    return rtn
 
 
 if __name__ == "__main__":
