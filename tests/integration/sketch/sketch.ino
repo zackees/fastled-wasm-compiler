220c44fc632cdbdc7a714ebaf40538ef1210be32
zackees <z@zackees.com>
2025-05-17 21:54:27 -0700

    Add xutility to files.associations and implement audio processing

--- tests/integration/sketch/sketch.ino
+++ tests/integration/sketch/sketch.ino
@@ -1,6 +1,24 @@
 
 #include <FastLED.h>
 
+#include "fl/audio.h"
+#include "fl/math.h"
 
-void setup(){}
-void loop(){}
+UIAudio audio("Audio");
+
+void setup() {}
+void loop()
+{
+
+    while (AudioSample sample = audio.next())
+    {
+        for (int i = 0; i < sample.pcm().size(); ++i)
+        {
+            int32_t x = ABS(sample.pcm()[i]);
+            if (x > max)
+            {
+                max = x;
+            }
+        }
+    }
+}
