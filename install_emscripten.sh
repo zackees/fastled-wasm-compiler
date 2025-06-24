a8db4386b2ab653fdc0791ba7d526fdbf3420d81
zackees <z@zackees.com>
2025-06-24 16:19:30 -0700

    Add artifact listing and copying functionality

--- install_emscripten.sh
+++ install_emscripten.sh
@@ -18,5 +18,19 @@ source ./emsdk_env.sh
 emcc -v
 
 cd ..
-ARTIFACT_NAME="emsdk-$(uname | tr '[:upper:]' '[:lower:]')"
+
+# Map uname output to GitHub Actions OS names for consistency
+case "$(uname | tr '[:upper:]' '[:lower:]')" in
+  linux) OS_NAME="ubuntu-latest" ;;
+  darwin) OS_NAME="macos-latest" ;;
+  mingw*|cygwin*|msys*) OS_NAME="windows-latest" ;;
+  *) OS_NAME="$(uname | tr '[:upper:]' '[:lower:]')" ;;
+esac
+
+ARTIFACT_NAME="emsdk-${OS_NAME}"
 tar -czf "${ARTIFACT_NAME}.tar.gz" emsdk
+
+# Also create the artifact in the original repository directory if we're not already there
+if [ "$PWD" != "$GITHUB_WORKSPACE" ] && [ -n "$GITHUB_WORKSPACE" ]; then
+    cp "${ARTIFACT_NAME}.tar.gz" "$GITHUB_WORKSPACE/"
+fi
